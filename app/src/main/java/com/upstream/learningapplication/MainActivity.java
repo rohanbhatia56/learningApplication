63341f1a6c5db46e7b750eb4421112d6fd4836d8
Rohan Bhatia <rohan.bhatia@nagarro.com>
2020-12-12 14:18:01 +0530

    background services

---
+++ app/src/main/java/com/upstream/learningapplication/MainActivity.java
@@ -0,0 +1,15 @@
+package com.upstream.learningapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+    }
+}
